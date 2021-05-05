ad175622d1dcfd135ff490b4f0bd050e732d6a63
Vadim <arbitr38@gmail.com>
2021-05-05 17:38:37 +0800

    body parser urlencoded

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,29 @@
 
-
+import qs from 'qs'
 
 async function routes (fastify, options) {
-    fastify.get('/client/read', async (request, reply) => (
 
+  fastify.addContentTypeParser('application/x-www-form-urlencoded', function (request, payload, done) {
+    let body = ''
+    payload.on('data', function (data) {
+      body += data
+    })
+    payload.on('end', function () {
+      try {
+        const parsed = qs.parse(body)
+        done(null, parsed)
+      } catch (e) {
+        done(e)
+      }
+    })
+    payload.on('error', done)
+  })
+
+
+
+
+    fastify.get('/client/read', async (request, reply) => {
+reply.type('text/html').send(
         `
     
         <h1>Read</h1>
@@ -15,16 +35,17 @@ async function routes (fastify, options) {
         </form>
         
         
-        `)
-
+        `
+)
+    }
     )
 
     fastify.post('/client/write', async (request, reply) => {
 
         console.dir(request.body.name);
         
-        return (`
-        You've sent ${req.body.name}
+        reply.type('text/html').send(`
+        <p>You've sent ${request.body.name} </p>
         <a href="/client/read"><button>Back</button></a>
         `)
       })
