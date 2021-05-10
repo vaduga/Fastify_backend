45d98ad64bab31be51d747215d7f3bdf1615e4f3
vaduga <arbitr38@gmail.com>
2021-05-10 20:21:37 +0800

    file save multer demo

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 // Require the framework and instantiate it
-
-import {routes} from './routes/router.js'
-import f from 'fastify'
+import f from "fastify";
+import { routes } from "./routes/router.js";
 
 const fastify = f({
-    logger: true
-  })
-  
-  
-  fastify.register(routes)
+  logger: true,
+});
 
-  // Declare a route
- 
-  
-  // Run the server!
-  fastify.listen(3000, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    fastify.log.info(`server listening on ${address}`)
-  })
+fastify.register(routes);
+
+// Declare a route
+
+// Run the server!
+fastify.listen(3000, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+});
--- routes/router.js
+++ routes/router.js
@@ -1,58 +1,45 @@
+import qs from "qs";
 
-import qs from 'qs'
+import multer from "fastify-multer";
 
-async function routes (fastify, options) {
+const upload = multer({ dest: "./uploads" });
 
-  fastify.addContentTypeParser('application/x-www-form-urlencoded', function (request, payload, done) {
-    let body = ''
-    payload.on('data', function (data) {
-      body += data
-    })
-    payload.on('end', function () {
-      try {
-        const parsed = qs.parse(body)
-        done(null, parsed)
-      } catch (e) {
-        done(e)
-      }
-    })
-    payload.on('error', done)
-  })
-
-
-
-
-    fastify.get('/client/read', async (request, reply) => {
-reply.type('text/html').send(
-        `
+async function routes(fastify, options) {
+  fastify.register(multer.contentParser);
+  fastify.get("/client/read", async (request, reply) => {
+    reply.type("text/html").send(
+      `
     
         <h1>Read</h1>
-        <form action="/client/write" method="POST"> 
+        <form action="/client/write" method="POST" enctype="multipart/form-data"> 
     
         <input type="text" name="name"/>
+        <input type="file" name="avatar"/>
         <input type="submit"/>
         
         </form>
         
         
         `
-)
-    }
-    )
+    );
+  });
 
-    fastify.post('/client/write', async (request, reply) => {
+  fastify.post(
+    "/client/write",
+    { preHandler: upload.single("avatar") },
+    (request, reply) => {
+      console.dir(request.file);
 
-        console.dir(request.body.name);
-        
-        reply.type('text/html').send(`
+      console.log("heyey" + request.body.name);
+
+      reply.type("text/html").send(`
         <p>You've sent ${request.body.name} </p>
         <a href="/client/read"><button>Back</button></a>
-        `)
-      })
-
-
-    fastify.get('/', async (request, reply) => reply.redirect('/client/read'))
+        `);
     }
-  
-  
-  export {routes}
+  );
+
+  fastify.get("/", async (request, reply) => reply.redirect("/client/read"));
+}
+
+export { routes };
